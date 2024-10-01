b1dbfc78f68fe0e9f6b6a69456f651f6321b30e1
Nitaa1904 <nitafitrotul1904@gmail.com>
2024-10-01 11:43:27 +0700

    folder tugas

---
+++ PPB_Teory/02_Teory/main.dart
@@ -0,0 +1,21 @@
+// void main() {
+//   int namaMahasiswa = 10;
+//   print(namaMahasiswa);
+
+  
+
+//   // var dynamic
+
+//   var nama = "Nita";
+//   dynamic alamat = "Jl. Jendral Sudirman";
+
+//   print(nama);
+//   print(alamat);
+// }
+
+
+void main() {
+  List<dynamic> daftarMahasiswa = ["Nita", "Marah", 2, false, ["game", "coding"]];
+  print(daftarMahasiswa[4][1]);
+}
+
