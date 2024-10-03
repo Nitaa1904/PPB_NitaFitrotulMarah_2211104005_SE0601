05485fd050e491d7d0ad48400ace97f15f20cf12
Nitaa1904 <nitafitrotul1904@gmail.com>
2024-10-03 08:23:23 +0700

    feat : add variabel

---
+++ 03_Pengenalan_Dart/GUIDED/variabel.dart
@@ -0,0 +1,13 @@
+void main() {
+  var name = "Nita";
+  var age = "21";
+  print("name: $name, usia: $age");
+
+  // pengaman variabel menggunakan camelCase
+  String firstName, lastName; // Tipe data String
+  firstName = "Nita";
+  lastName = "Fitrotul";
+  final cuaca = "Hujan";
+
+  print("$firstName $lastName - Cuaca: $cuaca");
+}
